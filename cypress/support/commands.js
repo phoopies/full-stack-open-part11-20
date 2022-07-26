d9f5805561bcb680b397da58aa019b162e79cefd
phoopies <sotajagic@gmail.com>
2022-07-26 09:23:50 +0300

    fixed e2e commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,17 +25,17 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', ({ username, password }) => {
-  cy.request('POST', 'http://localhost:3000/api/login', {
+  cy.request('POST', '/api/login', {
     username, password,
   }).then(({ body }) => {
     localStorage.setItem('user', JSON.stringify(body))
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
 })
 
 Cypress.Commands.add('createBlog', ({ title, author, url }) => {
   cy.request({
-    url: 'http://localhost:3000/api/blogs',
+    url: '/api/blogs',
     method: 'POST',
     body: { title, author, url },
     headers: {
@@ -43,5 +43,5 @@ Cypress.Commands.add('createBlog', ({ title, author, url }) => {
     },
   })
 
-  cy.visit('http://localhost:3000')
+  cy.visit('/')
 })
